9bdb5944e3727fac2cb900f8fa4a36a6f837133a
raj-githubb <rajaryannisro@gmail.com>
2026-04-09 17:40:52 +0530

    m

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,7 +10,9 @@ const User = require("./models/user");
 const Category = require("./models/Category");
 const Slider = require("./models/Slider");
 const contactRoutes = require("./routes/contact");
+
 require("dotenv").config();
+
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(cors({ origin: "*" }));
@@ -28,9 +31,9 @@ const SECRET = "mysecretkey";
 
 
 //================= DATABASE =================
-mongoose.connect("mongodb://127.0.0.1:27017/mobile-shop")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+// mongoose.connect("mongodb://127.0.0.1:27017/mobile-shop")
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch((err) => console.log(err));
 
 // ================= FILE UPLOAD (ONLY ONCE) =================
 const storage = multer.diskStorage({
@@ -257,8 +260,11 @@ app.delete("/slider/:id", async (req, res) => {
   res.json({ message: "Deleted" });
 });
 
+
+
+
 // ================= SERVER =================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
