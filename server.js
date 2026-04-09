35df906ed5be39432a33ab5efa40874fee137826
raj-githubb <rajaryannisro@gmail.com>
2026-04-09 10:09:19 +0530

    changed app cores

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ const contactRoutes = require("./routes/contact");
 require("dotenv").config();
 const app = express();
 app.use(express.json({ limit: "10mb" }));
-app.use(cors());
-
+app.use(cors({
+  origin: "*"
+}));
 app.use("/contact", contactRoutes);
 
 
