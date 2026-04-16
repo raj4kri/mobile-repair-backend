4a84314e45e951ebc615b609cf564794574a345b
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2026-04-16 12:40:36 +0530

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,17 +2,20 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const header = req.headers.authorization;
-  if (!header) return res.status(401).json({ message: "No token" });
+
+  if (!header) {
+    return res.status(401).json({ message: "No token" });
+  }
+
   const token = header.split(" ")[1];
+
   try {
-    const decoded = jwt.verify(token, SECRET);
+    const decoded = jwt.verify(token, process.env.SECRET); // ✅ FIX
     req.user = decoded;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
+
 module.exports = authMiddleware;
-
-
-
