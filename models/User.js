8ca54298d5014fb4149919323e420a6abe2484d3
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2026-04-17 08:42:13 +0530

    Enhance User model with password hashing and role
    Added password hashing and role field to user schema.

--- models/User.js
+++ models/User.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: { type: String, unique: true },
   password: String,
+  role: { type: String, default: "user" }
 });
+
+// hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
