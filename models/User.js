5523dd5be79e39241e7cac91c4b77fc5202d0c30
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2026-04-18 13:45:57 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -8,12 +8,11 @@ const userSchema = new mongoose.Schema({
 });
 
 // hash password before save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
+  this.password = await bcrypt.hash(this.password, 10);
+});
 // compare password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
