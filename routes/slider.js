f8875c7b9765782398605a95d6763e0e03b9ffc1
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2026-04-18 15:23:22 +0530

    Add files via upload

--- routes/slider.js
+++ routes/slider.js
@@ -3,51 +3,59 @@ const Slider = require("../models/Slider");
 
 const router = express.Router();
 const authMiddleware = require("../middleware/auth");
-const upload = require("../middleware/upload");
-const uploadToCloudinary = require("../utils/cloudinary");
+const upload = require("../middleware/upload"); 
+const uploadToCloudinary  = require("../utils/cloudinary");
 
 console.log("authMiddleware TYPE:", typeof authMiddleware);
 console.log("upload.single TYPE:", typeof upload.single);
-router.post("/", authMiddleware, upload.single("image"), async (req, res) => {
-  try {
-    console.log("FILE:", req.file);
+router.post("/", authMiddleware ,upload.single("image"), async (req, res) => {   
+   try {
+    
+       console.log("FILE:", req.file);
     console.log("BODY:", req.body);
-
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
+  
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
+      }
+  
+      console.log("Uploading to cloudinary...");
+  
+     const result = await uploadToCloudinary(req.file.buffer, "slider");
+  
+      console.log("CLOUDINARY SUCCESS:", result.secure_url);
+  
+      const newSlider = new Slider({
+        image: result.secure_url,
+      });
+  
+      const saved = await newSlider.save();
+  
+      console.log("DB SAVED:", saved);
+  
+      res.json(saved);
+  
+    } catch (err) {
+      console.error("FINAL ERROR:", err);
+      res.status(500).json({ error: err.message });
     }
-
-    console.log("Uploading to cloudinary...");
-
-    const result = await uploadToCloudinary(req.file.buffer, "slider");
-
-    console.log("CLOUDINARY SUCCESS:", result.secure_url);
-    const slider = await Slider.create({
-      image: result.secure_url,
-    });
-
-    res.json(slider);
-  } catch (err) {
-    console.log("SLIDER ERROR:", err); // 🔥 MOST IMPORTANT
-    res.status(500).json({ message: err.message });
-  }
-});
+  });
 // router.post("/", (req, res) => {
 //   res.send("WORKING");
 // });
 
-router.get("/", async (req, res) => {
-  const data = await Slider.find();
-  res.json(data);
-});
 
-router.delete("/:id", authMiddleware, async (req, res) => {
-  try {
-    await Slider.findByIdAndDelete(req.params.id);
-    res.json({ message: "Deleted" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+  router.get("/", async (req, res) => {
+    const data = await Slider.find();
+    res.json(data);
+  });
+
+  router.delete("/:id", authMiddleware, async (req, res) => {
+    try {
+      await Slider.findByIdAndDelete(req.params.id);
+      res.json({ message: "Deleted" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }); 
 
 module.exports = router;
