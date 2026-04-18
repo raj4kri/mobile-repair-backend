356721c7ca2c265daf6805079f9220b50af207d0
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2026-04-18 14:06:33 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 require("dotenv").config();
-const JWT_SECRET = process.env.JWT_SECRET
+
 const app = express();
+
+// middleware
+app.use(cors());
 app.use(express.json());
 
-const User = require("./models/User");
-
-
-
-
-
-// ================= MIDDLEWARE =================
-app.use(cors({
-  origin: ["http://localhost:5173", "https://frontend-37cf.vercel.app"],
-  credentials: true,
-}));
-
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true }));
-
-// ================= CONFIG =================
-const PORT = process.env.PORT || 1000;
-const MONGO_URI = process.env.MONGO_URI;
-const SECRET = process.env.JWT_SECRET
-
-require("dotenv").config();
-
-// console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
-// ================= DATABASE =================
-mongoose.connect(MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-
-
-// ================= ROUTES =================
+// routes
 app.use("/auth", require("./routes/auth"));
 app.use("/slider", require("./routes/slider"));
 app.use("/products", require("./routes/product"));
@@ -47,9 +17,17 @@ app.use("/categories", require("./routes/categories"));
 app.use("/team", require("./routes/team"));
 app.use("/contact", require("./routes/contact"));
 
+// test route
+app.get("/", (req, res) => {
+  res.send("API running 🚀");
+});
 
+// DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-// ================= SERVER =================
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+// server
+app.listen(process.env.PORT || 1000, () => {
+  console.log("Server running");
 });
