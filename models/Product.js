a7c9276c96235993e4a4483faba4442c21c0d1f5
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2026-04-26 21:01:46 +0530

    Add files via upload

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String,
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
 
-  // 👇 multiple images
-  images: [
-    {
-      type: String,
-    },
-  ],
+    discount: { type: Number, default: 0 },
+    finalPrice: { type: Number, default: 0 },
+
+    category: String,
+    images: [String],
+  },
+  { timestamps: true }
+);
+
+productSchema.pre("save", function (next) {
+  const price = Number(this.price);
+  const discount = Number(this.discount || 0);
+
+  this.finalPrice = price - (price * discount) / 100;
+
+  next();
 });
+
 module.exports = mongoose.model("Product", productSchema);
