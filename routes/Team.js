f58746fa30f9fc7f48f1aa633ef97e8b312c0920
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2026-04-26 21:18:49 +0530

    Delete routes/Team.js

--- routes/Team.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const teamSchema = new mongoose.Schema({
-  image: String,
-  name: String,
-  role: String,
-});
-
-module.exports = mongoose.model("Team", teamSchema);
