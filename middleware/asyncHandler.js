f236e5bccca338c828a3a84e0dc0ced3ba61b843
raj-githubb <rajaryannisro@gmail.com>
2026-04-27 10:19:07 +0530

    fix mongoose hook

--- middleware/asyncHandler.js
+++ middleware/asyncHandler.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 module.exports = fn => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
-=======
-module.exports = fn => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
->>>>>>> 383f118593963a5e1911a521816c5e683ef6951f
 };
