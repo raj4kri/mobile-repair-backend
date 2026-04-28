233642bb6b96e096a0629a409cea1fa2bcf5813a
raj-githubb <rajaryannisro@gmail.com>
2026-04-28 21:13:58 +0530

    changed

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "https://frontend-37cf.vercel.app",
+    credentials: true,
+  })
+);
+
 const app = express();
 
 // middleware
