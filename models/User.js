f7553360362ab62f5af7292d5173de21d1002aae
raj-githubb <rajaryannisro@gmail.com>
2026-04-29 21:18:38 +0530

    updates

--- models/User.js
+++ models/User.js
@@ -18,8 +18,8 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ["superadmin", "admin", "manager"], // ✅ ADD THIS
-      default: "manager",
+      enum: ["superadmin", "admin", "manager", "user"], // ✅ ADD THIS
+      default: "user",
     },
 
     permissions: {
