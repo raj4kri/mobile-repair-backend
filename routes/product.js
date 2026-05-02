5f9cd43378ee9790529af0776dc7766e2c893401
raj-githubb <rajaryannisro@gmail.com>
2026-05-02 19:14:59 +0530

    prodect detail

--- routes/product.js
+++ routes/product.js
@@ -34,6 +34,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 // CREATE PRODUCT (only admin + superadmin)
 router.post(
   "/",
